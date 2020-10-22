974a75569db55cb52086683addf4dd7c1f9652f4
ehpc <ehpc@ehpc.io>
2020-10-22 16:42:45 +0300

    Fix

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-function createPromise(val) {
-  return Promise.resolve(val);
-}
-
-function createFailedPromise() {
-  return Promise.reject(new Error('fail'));
-}
-
-function waitFor(promise) {
-  return promise.then((val) => val * 2);
-}
-
-function waitForAll(promises) {
-  return Promise.all(promises).then((results) => results.map((x) => x * 3));
-}
-
-function waitForFirstOne(promises) {
-  return Promise.race(promises).then((val) => val * 4);
-}
 
 module.exports = {
   createPromise,
