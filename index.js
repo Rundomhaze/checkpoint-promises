18e09bdd9377274e4fbbdc1ffc0e9c588d41e9c1
Alex <petenkov2822@gmail.com>
2022-06-20 10:06:11 +0300

    Done

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+function createPromise(data) {
+  return new Promise((resolve) => {
+    resolve(data)
+  });
+};
+
+function createFailedPromise(){
+  return Promise.reject(new Error('fail'))
+};
+
+function waitFor(prom) {
+  return new Promise((resolve) => {
+    resolve(prom)
+  })
+  .then((data) => data * 2)
+};
+
+function waitForAll(arrProms) {
+  return Promise.all((arrProms))  
+  .then((data) => data.map((el) => el * 3))
+};
+
+function waitForFirstOne(arrProms){
+  return Promise.race((arrProms))
+  .then((data) => data * 4)
+};
+
+
 
 module.exports = {
   createPromise,
